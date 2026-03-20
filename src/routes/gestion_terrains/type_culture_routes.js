86272b5902263a8fcbb286112fa3c11fadfb7bcc
IgirubElam <igirubuntuelam@gmail.com>
2026-03-20 12:33:30 +0200

    some added code about documentation

--- src/routes/gestion_terrains/type_culture_routes.js
+++ src/routes/gestion_terrains/type_culture_routes.js
@@ -1,10 +1,142 @@
 const typeCultureRouter = require('express').Router();
 const TypeCultureController = require('../../controllers/gestion_terrains/TypeCulture_controller')
 
+/**
+ * @swagger
+ * /type_cultures:
+ *   get:
+ *     summary: Récupérer la liste des type de cultures
+ *     description: Retourne tous les type de cultures avec possibilité de recherche
+ *     tags:
+ *       - TypeCulture
+ *     parameters:
+ *       - in: query
+ *         name: search
+ *         schema:
+ *           type: string
+ *         description: Recherche par intitulé
+ *     responses:
+ *       200:
+ *         description: Liste récupérée avec succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 httpStatus:
+ *                   type: integer
+ *                   example: 200
+ *                 message:
+ *                   type: string
+ *                   example: Les type de cultures recupérés avec succès
+ *                 data:
+ *                   type: object
+ */
 typeCultureRouter.get('/type_cultures', TypeCultureController.getTypeCultures);
+
+/**
+ * @swagger
+ * /type_cultures:
+ *   post:
+ *     summary: Créer un type de culture
+ *     tags:
+ *       - TypeCulture
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - intitule
+ *             properties:
+ *               intitule:
+ *                 type: string
+ *                 example: Grains
+ *     responses:
+ *       200:
+ *         description: Type culture créé avec succès
+ *       422:
+ *         description: Erreur de validation
+ */
 typeCultureRouter.post('/type_cultures', TypeCultureController.createTypeCulture);
+
+/**
+ * @swagger
+ * /type_culture/{id_type_culture}:
+ *   get:
+ *     summary: Récupérer un type de culture
+ *     tags:
+ *       - TypeCulture
+ *     parameters:
+ *       - in: path
+ *         name: id_type_culture
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID du type culture
+ *     responses:
+ *       200:
+ *         description: Type culture trouvé
+ *       404:
+ *         description: Type culture non trouvé
+ */
 typeCultureRouter.get('/type_culture/:id_type_culture', TypeCultureController.getTypeCulture);
+
+/**
+ * @swagger
+ * /type_culture/{id_type_culture}:
+ *   put:
+ *     summary: Modifier un type de culture
+ *     tags:
+ *       - TypeCulture
+ *     parameters:
+ *       - in: path
+ *         name: id_type_culture
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               intitule:
+ *                 type: string
+ *                 example: Grains
+ *     responses:
+ *       200:
+ *         description: Type de culture modifié
+ *       404:
+ *         description: Non trouvé
+ */
 typeCultureRouter.put('/type_culture/:id_type_culture', TypeCultureController.updateTypeCulture);
+
+/**
+ * @swagger
+ * /type_culture/delete:
+ *   delete:
+ *     summary: Supprimer un ou plusieurs type de cultures
+ *     tags:
+ *       - TypeCulture
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               ids_type_cultures:
+ *                 type: string
+ *                 example: "[1,2,3]"
+ *     responses:
+ *       200:
+ *         description: Type de cultures supprimés
+ *       404:
+ *         description: Non trouvé
+ */
 typeCultureRouter.delete('/type_culture/delete', TypeCultureController.deleteTypeCulture);
 
 module.exports = typeCultureRouter;
--- src/routes/gestion_terrains/type_engrais_routes.js
+++ src/routes/gestion_terrains/type_engrais_routes.js
@@ -1,10 +1,142 @@
 const typeEngraisRouter = require('express').Router();
 const TypeEngraisController = require('../../controllers/gestion_terrains/TypeEngrais_controller')
 
-typeEngraisRouter.get('/type_cultures', TypeEngraisController.getTypeEngrais);
-typeEngraisRouter.post('/type_cultures', TypeEngraisController.createTypeEngrais);
-typeEngraisRouter.get('/type_culture/:id_type_culture', TypeEngraisController.getOneTypeEngrais);
-typeEngraisRouter.put('/type_culture/:id_type_culture', TypeEngraisController.updateTypeEngrais);
-typeEngraisRouter.delete('/type_culture/delete', TypeEngraisController.deleteTypeEngrais);
+/**
+ * @swagger
+ * /type_engrais:
+ *   get:
+ *     summary: Récupérer la liste des type d'engrais
+ *     description: Retourne tous les type d'engrais avec possibilité de recherche
+ *     tags:
+ *       - TypeEngrais
+ *     parameters:
+ *       - in: query
+ *         name: search
+ *         schema:
+ *           type: string
+ *         description: Recherche par intitulé
+ *     responses:
+ *       200:
+ *         description: Liste récupérée avec succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 httpStatus:
+ *                   type: integer
+ *                   example: 200
+ *                 message:
+ *                   type: string
+ *                   example: Les type d'engrais recupérés avec succès
+ *                 data:
+ *                   type: object
+ */
+typeEngraisRouter.get('/type_engrais', TypeEngraisController.getTypeEngrais);
+
+/**
+ * @swagger
+ * /type_engrais:
+ *   post:
+ *     summary: Créer un type d'engrais
+ *     tags:
+ *       - TypeEngrais
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - intitule
+ *             properties:
+ *               intitule:
+ *                 type: string
+ *                 example: Ifumbire
+ *     responses:
+ *       200:
+ *         description: Type engrais créé avec succès
+ *       422:
+ *         description: Erreur de validation
+ */
+typeEngraisRouter.post('/type_engrais', TypeEngraisController.createTypeEngrais);
+
+/**
+ * @swagger
+ * /type_engrais/{id_type_engrais}:
+ *   get:
+ *     summary: Récupérer un type d'engrais
+ *     tags:
+ *       - TypeEngrais
+ *     parameters:
+ *       - in: path
+ *         name: id_type_engrais
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID du type engrais
+ *     responses:
+ *       200:
+ *         description: Type engrais trouvé
+ *       404:
+ *         description: Type engrais non trouvé
+ */
+typeEngraisRouter.get('/type_engrais/:id_type_engrais', TypeEngraisController.getOneTypeEngrais);
+
+/**
+ * @swagger
+ * /type_engrais/{id_type_engrais}:
+ *   put:
+ *     summary: Modifier un type d'engrais
+ *     tags:
+ *       - TypeEngrais
+ *     parameters:
+ *       - in: path
+ *         name: id_type_engrais
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               intitule:
+ *                 type: string
+ *                 example: Ifumbire
+ *     responses:
+ *       200:
+ *         description: Type d'engrais modifié
+ *       404:
+ *         description: Non trouvé
+ */
+typeEngraisRouter.put('/type_engrais/:id_type_engrais', TypeEngraisController.updateTypeEngrais);
+
+/**
+ * @swagger
+ * /type_engrais/delete:
+ *   delete:
+ *     summary: Supprimer un ou plusieurs type d'engrais
+ *     tags:
+ *       - TypeEngrais
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               ids_type_engrais:
+ *                 type: string
+ *                 example: "[1,2,3]"
+ *     responses:
+ *       200:
+ *         description: Type d'engrais supprimés
+ *       404:
+ *         description: Non trouvé
+ */
+typeEngraisRouter.delete('/type_engrais/delete', TypeEngraisController.deleteTypeEngrais);
 
 module.exports = typeEngraisRouter;

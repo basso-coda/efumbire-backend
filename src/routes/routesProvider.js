2739c6daf0c147203aec0a99bb338dce6abbe602
IgirubElam <igirubuntuelam@gmail.com>
2026-03-20 12:36:41 +0200

    other adds

--- src/routes/routesProvider.js
+++ src/routes/routesProvider.js
@@ -3,11 +3,13 @@ const mainRouter = require('express').Router();
 const agriculteurRouter = require('./gestion_agriculteurs/agriculteur_routes');
 const typeAgriculteurRouter = require('./gestion_agriculteurs/type_agriculteur_routes');
 const saisonRouter = require('./gestion_commandes/saison_routes');
+const typeCultureRouter = require('./gestion_terrains/type_culture_routes');
 const typeEngraisRouter = require('./gestion_terrains/type_engrais_routes');
 
 mainRouter.use(typeAgriculteurRouter);
 mainRouter.use(saisonRouter);
 mainRouter.use(typeEngraisRouter);
 mainRouter.use(agriculteurRouter);
+mainRouter.use(typeCultureRouter)
 
 module.exports = mainRouter;
